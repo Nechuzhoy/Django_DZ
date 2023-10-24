f152f29f41e8683111f689fe44479fa4db30698c
DDoS <9475098@gmail.com>
2023-10-24 18:09:05 +0300

    changed serializers.py, views.py, settings.py

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,30 @@
 from rest_framework import serializers
 
+from .models import Product, Stock, StockProduct
+
 
 class ProductSerializer(serializers.ModelSerializer):
     # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product  # Модель
+        fields = '__all__'
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct  # Модель
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
     # настройте сериализатор для склада
+    class Meta:
+        model = Stock  # Модель
+        # fields = '__all__'
+        fields = ['address', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -22,11 +32,14 @@ class StockSerializer(serializers.ModelSerializer):
 
         # создаем склад по его параметрам
         stock = super().create(validated_data)
-
+        print(validated_data)
+        print(stock)
+        print(positions)
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for i in positions:
+            StockProduct.objects.create(stock=stock, **i)
         return stock
 
     def update(self, instance, validated_data):
@@ -39,5 +52,11 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for i in positions:
+            obj, created = StockProduct.objects.update_or_create(
+                stock=stock,
+                product=i['product'],
+                defaults={'stock': stock, 'product': i['product'], 'quantity': i['quantity'],
+                          'price': i['price']}
+            )
         return stock
